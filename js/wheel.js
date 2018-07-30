6887b7e1dcdc92cf08c457c7f99481ecd59c8b08
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-07-30 19:32:13 +0100

    fixed defaultGameData variable

--- js/wheel.js
+++ js/wheel.js
@@ -1,9 +1,9 @@
 const wheel = (function() {
   const defaultGameData = { lifes: 5, difficulty: "easy", ia: "false" };
 
-  const startGame = (params = gameData) => {
+  const startGame = (params = defaultGameData) => {
     if (params instanceof Object && Object.keys(params).length >= 0) {
-      Object.assign(params, gameData);
+      Object.assign(params, defaultGameData);
     }
   };
 
