214e837738ef402bb7b3443bc810cfd8e339a923
Ranacode <romerodeveloper@techie.com>
2018-07-30 19:51:17 +0100

    Created phrases.json and function to allow us fetch the phrases for the game

--- js/wheel.js
+++ js/wheel.js
@@ -1,18 +1,47 @@
 const wheel = (function() {
   const defaultGameData = { lifes: 5, difficulty: "easy", ia: "false" };
 
-  const startGame = (params = gameData) => {
-    if (params instanceof Object && Object.keys(params).length >= 0) {
-      Object.assign(params, gameData);
+  const startGame = (gameData = defaultGameData) => {
+    if (gameData instanceof Object && Object.keys(gameData).length >= 0) {
+      gameData["phrases"] = fetchPhrases();
+      console.log(gameData);
     }
+  };
+
+  const fetchPhrases = () => {
+    let phrases;
+    if (!localStorage.getItem("phrases")) {
+      const { host, protocol } = window.location;
+      const url = `${protocol}//${host}/js/phrases/phrases.json`;
+      fetch(url, {
+        method: "GET",
+        mode: "same-origin",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        referrer: "no-referrer"
+      })
+        .then(res => res.json())
+        .then(data => {
+          phrases = data;
+          localStorage.setItem("phrases", JSON.stringify(phrases));
+        })
+        .catch(err => console.log(err));
+    } else {
+      phrases = JSON.parse(localStorage.getItem("phrases"));
+    }
+    return phrases;
   };
 
   const randomQueryGenerator = function() {};
 
-  const isValidUserInput = () =>
-    typeof letter === "string" && letter.length > 0;
+  const isValidUserInput = userInput =>
+    typeof letter === "string" && letter.length === 1;
 
-  const checkUserInput = function(letter) {};
+  const checkUserInput = function(letter) {
+    if (isValidUserInput(letter)) {
+    }
+  };
   return {
     start: startGame,
     checkInput: checkUserInput
