fc33a30ae58d23ee3d0d67b40c3ec8f5762d6a45
Ranacode <romerodeveloper@techie.com>
2018-07-31 16:30:39 +0100

    New function prepareGameData() that allow us setup the game rules

--- js/wheel.js
+++ js/wheel.js
@@ -1,38 +1,48 @@
 const wheel = (function() {
-  const defaultGameData = { lifes: 5, difficulty: "easy", ia: "false" };
+  const gameState = {};
 
-  const startGame = (gameData = defaultGameData) => {
+  const startGame = (
+    gameData = { lifes: 5, difficulty: "easy", ia: "false" }
+  ) => {
     if (gameData instanceof Object && Object.keys(gameData).length >= 0) {
+      Object.assign(gameState, gameData);
+      console.log(gameState);
       if (!localStorage.getItem("phrases")) {
-        fetchPhrases();
-        //ASIGNACION DE FRASES TEMPORAL
-        setTimeout(() => {
-          gameData["phrases"] = JSON.parse(localStorage.getItem("phrases"));
-        }, 150);
+        fetchPhrases()
+          .then(phrases => {
+            localStorage.setItem("phrases", JSON.stringify(phrases));
+            prepareGameData();
+          })
+          .catch(err => console.log(err));
       }
-      console.log(gameData);
+      prepareGameData();
     }
+  };
+
+  const prepareGameData = function() {
+    gameState["phrases"] = JSON.parse(localStorage.getItem("phrases"));
+    gameState["phrase"] = randomQueryGenerator();
+  };
+
+  const randomQueryGenerator = function() {
+    const phrases = gameState["phrases"];
+    const randomPhrase =
+      phrases[Math.round(Math.random() * phrases.length - 1)];
+    console.log(randomPhrase);
   };
 
   const fetchPhrases = () => {
     const { host, protocol } = window.location;
     const url = `${protocol}//${host}/js/phrases/phrases.json`;
-    fetch(url, {
+    return fetch(url, {
       method: "GET",
       mode: "same-origin",
       headers: {
         "Content-Type": "application/json; charset=utf-8"
       },
       referrer: "no-referrer"
-    })
-      .then(res => res.json())
-      .then(phrases => {
-        localStorage.setItem("phrases", JSON.stringify(phrases));
-      })
-      .catch(err => console.log(err));
+    }).then(res => res.json());
   };
-
-  const randomQueryGenerator = function() {};
 
   const isValidUserInput = userInput =>
     typeof letter === "string" && letter.length === 1;
